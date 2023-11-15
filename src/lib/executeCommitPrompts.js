e27ca15d41cbbb51882991c6044b660346cf9dc7
Ubuntu <ubuntu@ip-172-31-25-41.eu-north-1.compute.internal>
2023-11-15 03:07:49 +0000

    fix (../executeCommitPrompts.js) - moved process.exitCode & rl.close() to the end of writeCommits fn.

--- src/lib/executeCommitPrompts.js
+++ src/lib/executeCommitPrompts.js
@@ -24,30 +24,30 @@ const getUserCommitCategoryInput = async (choice, rl) => {
  * @param {readline.Interface} rl - The readline interface for user input.
  */
 async function writeCommits(commitMsg, rl) {
-  try {
-    // Make a local commit
-    await writeLocalCommit(commitMsg, rl);
+	try {
+		// Make a local commit
+		await writeLocalCommit(commitMsg, rl);
 
-    // Ask user to commit to remote origin
-    const remoteCommitConfirm = await validateUserInput("Push commit to remote origin? (Y | N)", rl, "ORIGIN");
+		// Ask user to commit to remote origin
+		const remoteCommitConfirm = await validateUserInput("Push commit to remote origin? (Y | N)", rl, "ORIGIN");
 
 		// Commit to remote origin
 		if (["yes", "y"].includes(remoteCommitConfirm.toLowerCase())) {
-      const remoteCommitOk = await writeRemoteCommit(rl);
-      if (!remoteCommitOk) {
-        forceRemoteCommit(rl);
-      }
-    }
-
-    // Set exit code and close readline interface
-    process.exitCode = 0;
-    rl.close();
-  } catch (error) {
-    console.error("An error occurred:", error);
-    // Optionally, set a non-zero exit code if an error occurs
-    process.exitCode = 1;
-    rl.close();
-  }
+			const remoteCommitOk = await writeRemoteCommit(rl);
+			if (!remoteCommitOk) {
+				forceRemoteCommit(rl);
+			}
+		}
+	} catch (error) {
+		console.error(chalk.warningStrong("An error occurred:"));
+		console.error({ error })
+		// Optionally, set a non-zero exit code if an error occurs
+		process.exitCode = 1;
+		rl.close();
+	}
+	// Set exit code and close readline interface
+	process.exitCode = 0;
+	rl.close();
 }
 
 /**
