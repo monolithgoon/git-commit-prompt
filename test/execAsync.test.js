8aa12779daabb8f704f5a7a0d0fb940ef889d294
monolithgoon <svanteahrrenus@hotmail.com>
2023-11-16 08:41:51 +0000

    implemented valid tests for execAsync.js

--- test/execAsync.test.js
+++ test/execAsync.test.js
@@ -1,40 +1,36 @@
-const { exec } = require('child_process');
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-const { execAsync } = require('../src/execAsync');
-
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const { execAsync } = require("../src/execAsync");
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-describe('execAsync function', () => {
-  it('should resolve with stdout when the command is successful', async () => {
-    const mockStdout = 'Command executed successfully';
-    exec.yields(null, mockStdout, null);
+describe("execAsync function", () => {
 
-    const result = await execAsync('mock command');
-    expect(result).to.equal(mockStdout);
-  });
+	// 1.
+	it("should resolve with stdout when the command succeeds", async () => {
+		const result = await execAsync(`echo Hello, Mocha`);
+		expect(result).to.equal("Hello, Mocha\n");
+	});
 
-  it('should reject with an error when the command fails', async () => {
-    const mockError = new Error('Command failed');
-    exec.yields(mockError, null, null);
+	// 2.
+	it("should reject with an error when the command fails", async () => {
+		try {
+			await execAsync("nonexistent-command");
+			// If the command succeeds, the test should fail
+			expect.fail("Expected execAsync to reject but it resolved");
+		} catch (error) {
+			expect(error).to.be.an("error");
+		}
+	});
 
-    await expect(execAsync('mock command')).to.be.rejectedWith(mockError);
-  });
-
-  it('should reject with stderr when there is an error', async () => {
-    const mockStderr = 'Error occurred';
-    exec.yields(null, null, mockStderr);
-
-    await expect(execAsync('mock command')).to.be.rejectedWith(mockStderr);
-  });
-
-  it('should close the readline interface if provided when there is an error', async () => {
-    const mockError = new Error('Command failed');
-    const mockReadline = { close: () => {} };
-    exec.yields(mockError, null, null);
-
-    await expect(execAsync('mock command', mockReadline)).to.be.rejectedWith(mockError);
-    expect(mockReadline.close).to.have.been.calledOnce;
-  });
+	// 3.
+	it("should reject with stderr when the command has stderr output", async () => {
+		try {
+			await execAsync('echo "Error message" 1>&2');
+			// If the command succeeds, the test should fail
+			expect.fail("Expected execAsync to reject but it resolved");
+		} catch (error) {
+			expect(error).to.equal("Error message\n");
+		}
+	});
 });
--- test/validasteUserInput.test.js
+++ test/validasteUserInput.test.js
@@ -1,60 +1,60 @@
-const { expect } = require("chai");
-const readline = require("readline");
-const { validateUserInput } = require("../src/validateUserInput");
+// const { expect } = require("chai");
+// const readline = require("readline");
+// const { validateUserInput } = require("../src/validateUserInput");
 
-// Create a readline interface using the readable stream
-const rlMock = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// // Create a readline interface using the readable stream
+// const rlMock = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-describe("validateUserInput", () => {
-  it("should validate commit type input", async () => {
-    const promptMsg = "Enter commit type:";
-    const promptFlag = "TYPE";
-    const validInput = "feat";
-    const invalidInput = "a";
+// describe("validateUserInput", () => {
+//   it("should validate commit type input", async () => {
+//     const promptMsg = "Enter commit type:";
+//     const promptFlag = "TYPE";
+//     const validInput = "type";
+//     const invalidInput = "feat";
 
-    // Valid input
-    rlMock.question = (query, callback) => callback(validInput);
-    const result = await validateUserInput(promptMsg, rlMock, promptFlag);
-    expect(result).to.equal(validInput);
+//     // Valid input
+//     rlMock.question = (query, callback) => callback(validInput);
+//     const result = await validateUserInput(promptMsg, rlMock, promptFlag);
+//     expect(result).to.equal(validInput);
 
-    // Invalid input (too short)
-    rlMock.question = (query, callback) => callback(invalidInput);
-    const invalidResult = await validateUserInput(promptMsg, rlMock, promptFlag);
-    expect(invalidResult).to.not.equal(validInput);
-  });
+//     // Invalid input (too short)
+//     rlMock.question = (query, callback) => callback(invalidInput);
+//     const invalidResult = await validateUserInput(promptMsg, rlMock, promptFlag);
+//     expect(invalidResult).to.not.equal(validInput);
+//   });
 
-  it("should validate commit domain input", async () => {
-    const promptMsg = "Enter commit domain:";
-    const promptFlag = "DOMAIN";
-    const validInput = "example";
-    const invalidInput = "ab";
+//   it("should validate commit domain input", async () => {
+//     const promptMsg = "Enter commit domain:";
+//     const promptFlag = "DOMAIN";
+//     const validInput = "example";
+//     const invalidInput = "fuckyouabsolutetwat";
 
-    // Valid input
-    rlMock.question = (query, callback) => callback(validInput);
-    const result = await validateUserInput(promptMsg, rlMock, promptFlag);
-    expect(result).to.equal(validInput);
+//     // Valid input
+//     rlMock.question = (query, callback) => callback(validInput);
+//     const result = await validateUserInput(promptMsg, rlMock, promptFlag);
+//     expect(result).to.equal(validInput);
 
-    // Invalid input (too short)
-    rlMock.question = (query, callback) => callback(invalidInput);
-    const invalidResult = await validateUserInput(promptMsg, rlMock, promptFlag);
-    expect(invalidResult).to.not.equal(validInput);
-  });
+//     // Invalid input (too short)
+//     rlMock.question = (query, callback) => callback(invalidInput);
+//     const invalidResult = await validateUserInput(promptMsg, rlMock, promptFlag);
+//     expect(invalidResult).to.not.equal(validInput);
+//   });
 
-  // Add similar tests for other promptFlags
+//   // Add similar tests for other promptFlags
 
-  // ...
+//   // ...
 
-  it("should handle invalid input and retry", async () => {
-    const promptMsg = "Enter commit type:";
-    const promptFlag = "TYPE";
-    const invalidInput = "";
+//   it("should handle invalid input and retry", async () => {
+//     const promptMsg = "Enter commit type:";
+//     const promptFlag = "TYPE";
+//     const invalidInput = "";
 
-    rlMock.question = (query, callback) => callback(invalidInput);
-    const result = await validateUserInput(promptMsg, rlMock, promptFlag);
-    // Since the input is invalid, it should retry and eventually return a valid result
-    expect(result).to.not.equal(invalidInput);
-  });
-});
+//     rlMock.question = (query, callback) => callback(invalidInput);
+//     const result = await validateUserInput(promptMsg, rlMock, promptFlag);
+//     // Since the input is invalid, it should retry and eventually return a valid result
+//     expect(result).to.not.equal(invalidInput);
+//   });
+// });
