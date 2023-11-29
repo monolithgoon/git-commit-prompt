f8be166975c38ecbe5d01b7d1831547d2c42a89f
Ubuntu <ubuntu@ip-172-31-25-146.eu-north-1.compute.internal>
2023-11-29 11:22:52 +0000

    [docs] (src/lib/_createPrompts.js) - added jsdocs and line comments

--- src/lib/_createPrompts.js
+++ src/lib/_createPrompts.js
@@ -1,6 +1,12 @@
 /* eslint-disable import/no-dynamic-require, global-require */
 // Disable eslint rules for dynamic require and global require
 
+/**
+ * Object mapping different prompt types to their respective modules.
+ * @typedef {Object} PromptCreators
+ * @property {Function} scope - Function to create a scope prompt.
+ * @property {Function} type - Function to create a type prompt.
+ */
 // const qBody = require('./_prompts/body');
 // const qBreaking = require('./_prompts/breaking');
 // const qIssues = require('./_prompts/issues');
@@ -18,32 +24,44 @@ const promptCreators = {
 	// subject: qSubject,
 	type: promptType,
 };
-// Object mapping different prompt types to their respective modules
 
-const createPrompts = (state, cliAnswers) => {
-	// console.log({ state })
-	// console.log({ cliAnswers });
-	console.log(state.config.promptKeys);
+/**
+ * Creates an array of prompts based on the state and CLI prompt flags.
+ *
+ * @function createPrompts
+ * @memberof module:PromptCreators
+ * @param {Object} state - The state object containing session information.
+ * @param {Object} cliPromptFlags - Flags provided through the command line for prompts.
+ * @returns {Array<Object>} An array of prompts to be used with Inquirer.
+ */
+const createPrompts = (state, cliPromptFlags) => {
+  // Console logs are left in the code, likely for debugging purposes
+  console.log(state.config.promptKeys);
 
-	const prompts = state.config.promptKeys
-		// Filter out prompt keys with existing CLI args
-		.filter((promptKey) => cliAnswers[promptKey] === undefined)
+  const prompts = state.config.promptKeys
+		// Filter out prompt keys for which CLI args already exist
+    .filter((promptKey) => cliPromptFlags[promptKey] === undefined)
 
-		.map((promptKey, idx) => {
-			console.log({ promptKey, idx });
-			const prompt = promptCreators[promptKey].createPrompt(state);
+    .map((promptKey, idx) => {
+      console.log({ promptKey, idx });
+      // Get the prompt creator function for the current promptKey
+      const promptCreator = promptCreators[promptKey];
 
-			// **** Todo
-			// Set custom messages for prompts if configured
-			if (state.config.messages && state.config.messages[promptKey]) {
-				prompt.message = state.config.messages[promptKey];
-			}
+      // Create the prompt using the prompt creator function
+      const prompt = promptCreator.createPrompt(state);
 
-			return prompt;
-		});
+      // **** Todo
+      // Set custom messages for prompts if configured
+      if (state.config.messages && state.config.messages[promptKey]) {
+        prompt.message = state.config.messages[promptKey];
+      }
 
-	// Filter out any undefined prompts before returning
-	return prompts.filter(Boolean);
+      return prompt;
+    });
+
+  // Filter out any undefined prompts before returning
+  return prompts.filter(Boolean);
 };
 
+// Export the function for use in other modules
 module.exports = createPrompts;
