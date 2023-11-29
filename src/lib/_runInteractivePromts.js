626fc63111a65f2442759e02f974b9a5e174a201
Ubuntu <ubuntu@ip-172-31-25-146.eu-north-1.compute.internal>
2023-11-29 11:12:04 +0000

    [docs] (src/lib/_runInteractivePrompts.js) - added jsdocs and line comments

--- src/lib/_runInteractivePromts.js
+++ src/lib/_runInteractivePromts.js
@@ -1,35 +1,52 @@
 const inquirer = require("inquirer");
 const ListSearchPrompt = require("inquirer-list-search-prompt");
-// const LimitedInputPrompt = require('./LimitedInputPrompt');
 const createPrompts = require("./_createPrompts");
 
-// inquirer.registerPrompt('limitedInput', LimitedInputPrompt);
+/**
+ * Registers the List Search prompt with Inquirer.
+ * @name registerListSearchPrompt
+ * @function
+ * @memberof module:InteractivePrompts
+ */
 inquirer.registerPrompt("list-search", ListSearchPrompt);
 
+/**
+ * Runs interactive prompts using Inquirer to gather user input.
+ *
+ * @async
+ * @function runInteractivePrompts
+ * @memberof module:InteractivePrompts
+ * @param {Object} state - The state object containing session information.
+ * @param {Object} cliAnswers - Answers provided through the command line.
+ * @returns {Promise<Object>} A Promise that resolves with the user's prompt responses.
+ */
 const runInteractivePrompts = async (state, cliAnswers = {}) => {
-	// Update the state with inputs from the CMD line
-	Object.keys(cliAnswers).forEach((key) => {
-		state.promptResponseData[key] = cliAnswers[key];
-	});
+  // Update the state with inputs from the CMD line
+  Object.keys(cliAnswers).forEach((key) => {
+    state.promptResponseData[key] = cliAnswers[key];
+  });
 
-	const prompts = createPrompts(state, cliAnswers);
+  // Generate prompts based on the state and CLI answers
+  const prompts = createPrompts(state, cliAnswers);
 
-	console.log({ prompts });
+  // Log generated prompts for debugging
+  console.log({ prompts });
 
-	const promptResponses = await inquirer.prompt(prompts);
+  // Use Inquirer to prompt the user with the generated prompts
+  const promptResponses = await inquirer.prompt(prompts);
 
-	console.log({ promptResponses });
+  // Log user responses for debugging
+  console.log({ promptResponses });
 
-	Object.keys(state.promptResponseData).forEach((promptCat) => {
-		// console.log({ promptCat });
-		// console.table(promptResponses[`${promptCat}`]);
+  // Update state with user responses
+  Object.keys(state.promptResponseData).forEach((promptCat) => {
+    if (promptResponses[promptCat]) {
+      state.promptResponseData[promptCat] = promptResponses[promptCat];
+    }
+  });
 
-		if (promptResponses[promptCat]) {
-			state.promptResponseData[promptCat] = promptResponses[promptCat];
-		}
-	});
-
-	return promptResponses;
+  return promptResponses;
 };
 
+// Export the function for use in other modules
 module.exports = runInteractivePrompts;
